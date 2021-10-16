2e4efb40b9467b95cc44ca7090af6b37e742f61e
idoam <idoamit1@mail.tau.ac.il>
2021-10-16 16:48:29 +0300

    first commit in new branch

--- src/First.java
+++ src/First.java
@@ -1,5 +1,20 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("Lets Practice Git");
+        // Learning Arithmetic Operators.
+        int a = 12;
+        int b = a++;
+        int c = 12;
+        int d = ++c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("-------------------");
+
+        a = 8;
+        b = 16;
+        a = a<<8;
+        System.out.println(a);
+
     }
 }
